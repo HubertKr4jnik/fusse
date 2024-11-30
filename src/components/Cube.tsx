23d1e190232bc3ae3eca446193aa476237e9d561
Hubert Krajnik <hkrajnik714@gmail.com>
2024-11-30 23:37:34 +0100

    add better UI, instructions and pseudo-random module generation

--- src/components/Cube.tsx
+++ src/components/Cube.tsx
@@ -32,7 +32,7 @@ export default function Cube() {
     return (
       <>
         {
-          !started && <div className="h-fit p-10 bg-slate-300 rounded flex flex-col place-items-center justify-evenly">
+          !started && <div className="h-fit p-10 bg-slate-300 rounded flex flex-col place-items-center justify-evenly shadow-2xl shadow-black border-4 border-slate-500">
           <div className="text-center pb-4">
             <h1 className="font-bold text-4xl">Welcome to "Fusse"</h1>
             <p>Please select your role:</p>
@@ -51,11 +51,27 @@ export default function Cube() {
           <h1 className="text-white font-bold text-3xl">Congratulations!</h1>
           <p className="text-white text-xl pt-4 text-center">You overcame yourself and defused the bomb</p>
         </>}
-        {wrong < 3 && started && wrong + correct != 8 && <>
+        {wrong < 3 && started && wrong + correct != 8 && random == 0 && <>
         <h1 className="pb-6 font-bold text-xl">
-          <p className="text-green-600 border-2 py-2 px-4 rounded-3xl">{8 - (correct + wrong)} module(s) left</p>
+          <p className="text-green-600 border-2 py-2 px-4 rounded-3xl shadow-lg shadow-black">{8 - (correct + wrong)} module(s) left</p>
         </h1>
-        <div className="aspect-square h-4/5 bg-white grid grid-cols-3 grid-rows-3 gap-1 p-1 rounded shadow-2xl">
+        <div className="aspect-square h-4/5 bg-white grid grid-cols-3 grid-rows-3 gap-1 p-1 rounded shadow-2xl shadow-black">
+          <Cables value={correct} handleChange={setCorrect} wrong={wrong} handleWrong={setWrong}/>
+          <Binary value={correct} handleChange={setCorrect} wrong={wrong} handleWrong={setWrong}/>
+          <Captcha value={correct} handleChange={setCorrect} wrong={wrong} handleWrong={setWrong}/>
+          <Display minutes={minutes} handleMinutes={setMinutes} seconds={seconds} handleSeconds={setSeconds} wrong={wrong} handleWrong={setWrong} correct={correct}/>
+          <Captcha value={correct} handleChange={setCorrect} wrong={wrong} handleWrong={setWrong}/>
+          <Binary value={correct} handleChange={setCorrect} wrong={wrong} handleWrong={setWrong}/>
+          <Cables value={correct} handleChange={setCorrect} wrong={wrong} handleWrong={setWrong}/>
+          <Binary value={correct} handleChange={setCorrect} wrong={wrong} handleWrong={setWrong}/>
+          <Cables value={correct} handleChange={setCorrect} wrong={wrong} handleWrong={setWrong}/>
+        </div>
+        </>}
+        {wrong < 3 && started && wrong + correct != 8 && random == 1 && <>
+        <h1 className="pb-6 font-bold text-xl">
+          <p className="text-green-600 border-2 py-2 px-4 rounded-3xl shadow-lg shadow-black">{8 - (correct + wrong)} module(s) left</p>
+        </h1>
+        <div className="aspect-square h-4/5 bg-white grid grid-cols-3 grid-rows-3 gap-1 p-1 rounded shadow-2xl shadow-black">
           <Cables value={correct} handleChange={setCorrect} wrong={wrong} handleWrong={setWrong}/>
           <Captcha value={correct} handleChange={setCorrect} wrong={wrong} handleWrong={setWrong}/>
           <Binary value={correct} handleChange={setCorrect} wrong={wrong} handleWrong={setWrong}/>
@@ -65,6 +81,70 @@ export default function Cube() {
           <Cables value={correct} handleChange={setCorrect} wrong={wrong} handleWrong={setWrong}/>
           <Binary value={correct} handleChange={setCorrect} wrong={wrong} handleWrong={setWrong}/>
           <Captcha value={correct} handleChange={setCorrect} wrong={wrong} handleWrong={setWrong}/>
+        </div>
+        </>}
+        {wrong < 3 && started && wrong + correct != 8 && random == 2 && <>
+        <h1 className="pb-6 font-bold text-xl">
+          <p className="text-green-600 border-2 py-2 px-4 rounded-3xl shadow-lg shadow-black">{8 - (correct + wrong)} module(s) left</p>
+        </h1>
+        <div className="aspect-square h-4/5 bg-white grid grid-cols-3 grid-rows-3 gap-1 p-1 rounded shadow-2xl shadow-black">
+          <Cables value={correct} handleChange={setCorrect} wrong={wrong} handleWrong={setWrong}/>
+          <Captcha value={correct} handleChange={setCorrect} wrong={wrong} handleWrong={setWrong}/>
+          <Captcha value={correct} handleChange={setCorrect} wrong={wrong} handleWrong={setWrong}/>
+          <Binary value={correct} handleChange={setCorrect} wrong={wrong} handleWrong={setWrong}/>
+          <Empty />
+          <Display minutes={minutes} handleMinutes={setMinutes} seconds={seconds} handleSeconds={setSeconds} wrong={wrong} handleWrong={setWrong} correct={correct}/>
+          <Cables value={correct} handleChange={setCorrect} wrong={wrong} handleWrong={setWrong}/>
+          <Binary value={correct} handleChange={setCorrect} wrong={wrong} handleWrong={setWrong}/>
+          <Cables value={correct} handleChange={setCorrect} wrong={wrong} handleWrong={setWrong}/>
+        </div>
+        </>}
+        {wrong < 3 && started && wrong + correct != 8 && random == 3 && <>
+        <h1 className="pb-6 font-bold text-xl">
+          <p className="text-green-600 border-2 py-2 px-4 rounded-3xl shadow-lg shadow-black">{8 - (correct + wrong)} module(s) left</p>
+        </h1>
+        <div className="aspect-square h-4/5 bg-white grid grid-cols-3 grid-rows-3 gap-1 p-1 rounded shadow-2xl shadow-black">
+          <Captcha value={correct} handleChange={setCorrect} wrong={wrong} handleWrong={setWrong}/>
+          <Display minutes={minutes} handleMinutes={setMinutes} seconds={seconds} handleSeconds={setSeconds} wrong={wrong} handleWrong={setWrong} correct={correct}/>
+          <Cables value={correct} handleChange={setCorrect} wrong={wrong} handleWrong={setWrong}/>
+          <Binary value={correct} handleChange={setCorrect} wrong={wrong} handleWrong={setWrong}/>
+          <Cables value={correct} handleChange={setCorrect} wrong={wrong} handleWrong={setWrong}/>
+          <Cables value={correct} handleChange={setCorrect} wrong={wrong} handleWrong={setWrong}/>
+          <Empty />
+          <Binary value={correct} handleChange={setCorrect} wrong={wrong} handleWrong={setWrong}/>
+          <Cables value={correct} handleChange={setCorrect} wrong={wrong} handleWrong={setWrong}/>
+        </div>
+        </>}
+        {wrong < 3 && started && wrong + correct != 8 && random == 4 && <>
+        <h1 className="pb-6 font-bold text-xl">
+          <p className="text-green-600 border-2 py-2 px-4 rounded-3xl shadow-lg shadow-black">{8 - (correct + wrong)} module(s) left</p>
+        </h1>
+        <div className="aspect-square h-4/5 bg-white grid grid-cols-3 grid-rows-3 gap-1 p-1 rounded shadow-2xl shadow-black">
+          <Empty />
+          <Cables value={correct} handleChange={setCorrect} wrong={wrong} handleWrong={setWrong}/>
+          <Binary value={correct} handleChange={setCorrect} wrong={wrong} handleWrong={setWrong}/>
+          <Captcha value={correct} handleChange={setCorrect} wrong={wrong} handleWrong={setWrong}/>
+          <Cables value={correct} handleChange={setCorrect} wrong={wrong} handleWrong={setWrong}/>
+          <Binary value={correct} handleChange={setCorrect} wrong={wrong} handleWrong={setWrong}/>
+          <Display minutes={minutes} handleMinutes={setMinutes} seconds={seconds} handleSeconds={setSeconds} wrong={wrong} handleWrong={setWrong} correct={correct}/>
+          <Binary value={correct} handleChange={setCorrect} wrong={wrong} handleWrong={setWrong}/>
+          <Cables value={correct} handleChange={setCorrect} wrong={wrong} handleWrong={setWrong}/>
+        </div>
+        </>}
+        {wrong < 3 && started && wrong + correct != 8 && random == 5 && <>
+        <h1 className="pb-6 font-bold text-xl">
+          <p className="text-green-600 border-2 py-2 px-4 rounded-3xl shadow-lg shadow-black">{8 - (correct + wrong)} module(s) left</p>
+        </h1>
+        <div className="aspect-square h-4/5 bg-white grid grid-cols-3 grid-rows-3 gap-1 p-1 rounded shadow-2xl shadow-black">
+          <Binary value={correct} handleChange={setCorrect} wrong={wrong} handleWrong={setWrong}/>
+          <Binary value={correct} handleChange={setCorrect} wrong={wrong} handleWrong={setWrong}/>
+          <Cables value={correct} handleChange={setCorrect} wrong={wrong} handleWrong={setWrong}/>
+          <Display minutes={minutes} handleMinutes={setMinutes} seconds={seconds} handleSeconds={setSeconds} wrong={wrong} handleWrong={setWrong} correct={correct}/>
+          <Empty />
+          <Captcha value={correct} handleChange={setCorrect} wrong={wrong} handleWrong={setWrong}/>
+          <Binary value={correct} handleChange={setCorrect} wrong={wrong} handleWrong={setWrong}/>
+          <Cables value={correct} handleChange={setCorrect} wrong={wrong} handleWrong={setWrong}/>
+          <Cables value={correct} handleChange={setCorrect} wrong={wrong} handleWrong={setWrong}/>
         </div>
         </>}
       </>
